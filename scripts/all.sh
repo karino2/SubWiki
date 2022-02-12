d455e4a7eb48ee26f1b7da3a9d0baef1c923b1eb
Kazuma Arino <hogeika2@gmail.com>
2022-02-12 12:39:02 +0900

    Remove redundant script and integrate recents_include generation.

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-./copy_and_conv.command
+SCRIPTS_DIR=$(dirname $0)
+
+cd $SCRIPTS_DIR
+./gen_recents_include.sh
+./copy.sh
+./conv.sh
 
 
 pushd ../; git add --all; git commit -m "update"; popd; ./gen_recents.sh; pushd ../; git add --all; git commit -m "update recents";  git push; popd;
